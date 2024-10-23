4a3aee51092a6ff8e869f8b97e8b99370314e583
itouuu777 <135014775+itouuu777@users.noreply.github.com>
2024-10-23 19:28:21 +0900

    Update blt2.ino

--- blt4/blt2.ino
+++ blt4/blt2.ino
@@ -1,29 +1,48 @@
-#include "SerialBT.h"
+#include <Adafruit_TinyUSB.h>
 
-SerialBT SerialBT;
+Adafruit_USBD_BLE ble;
+BLEConnection* connection;
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("PicoW_Server");  // サーバーデバイス名を設定
-  pinMode(32, OUTPUT);
-  digitalWrite(32, LOW);
-  Serial.println("Waiting for a connection...");
+  while (!Serial) {
+    delay(10); // シリアルポートが接続されるまで待つ
+  }
+  
+  // BLE初期化
+  if (!ble.begin()) {
+    Serial.println("Failed to start BLE");
+    while (1);
+  }
+  
+  ble.setDeviceName("PicoW_BLE_Client");
+  Serial.println("BLE client initialized, searching for server...");
+
+  // サーバーと接続を試みる
+  connection = ble.connect("PicoW_BLE_Server");
+  if (connection) {
+    Serial.println("Connected to BLE server");
+  } else {
+    Serial.println("Failed to connect to server");
+    while (1);
+  }
 }
 
 void loop() {
-  if (SerialBT.available()) {
-    byte inBuf[1];
-    SerialBT.readBytes(inBuf, 1);
-    Serial.print((char)inBuf[0]);
-    Serial.println("");
+  if (Serial.available()) {
+    char command = Serial.read();
 
-    if (inBuf[0] == 's') {
-      SerialBT.println("ECHO BACK >> LED ON");
-      digitalWrite(32, HIGH);
-    }
-    if (inBuf[0] == 'g') {
-      SerialBT.println("ECHO BACK >> LED OFF");
-      digitalWrite(32, LOW);
+    if (command == '1') {
+      Serial.println("Sending command: LED ON");
+      // サーバーにLED ONの値を送信
+      connection->write(1); 
+    } else if (command == '0') {
+      Serial.println("Sending command: LED OFF");
+      // サーバーにLED OFFの値を送信
+      connection->write(0);
     }
   }
+
+  // BLE接続の維持
+  ble.loop();
 }
